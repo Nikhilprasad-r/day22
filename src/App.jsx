b3d27e1bebf5014b74a26ad27443e19e55bf1815
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-08 11:01:27 +0530

    format

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,54 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./components/Card/Card";
 
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Card from './components/Card/Card';
-
-
-const data =[{
-  title:'FREE',
-  cost:0,
-  users:'single',
-  storage:true,
-  unlimited:true,
-  access:true,
-  private:false,
-  phone:false,
-  subdomain:false,
-  status:false
-},
-
-
-
-{
-  title:'PLUS',
-  cost:9,
-  users:'5',
-  storage:true,
-  unlimited:true,
-  access:true,
-  private:true,
-  phone:true,
-  subdomain:true,
-  status:false
-},{
-  title:'PRO',
-  cost:49,
-  users:'unlimited',
-  storage:true,
-  unlimited:true,
-  access:true,
-  private:true,
-  phone:true,
-  subdomain:true,
-  status:true
-}];
-
-
-
-
+const data = [
+  {
+    title: "FREE",
+    cost: 0,
+    users: "single",
+    storage: true,
+    unlimited: true,
+    access: true,
+    private: false,
+    phone: false,
+    subdomain: false,
+    status: false,
+  },
+  {
+    title: "PLUS",
+    cost: 9,
+    users: "5",
+    storage: true,
+    unlimited: true,
+    access: true,
+    private: true,
+    phone: true,
+    subdomain: true,
+    status: false,
+  },
+  {
+    title: "PRO",
+    cost: 49,
+    users: "unlimited",
+    storage: true,
+    unlimited: true,
+    access: true,
+    private: true,
+    phone: true,
+    subdomain: true,
+    status: true,
+  },
+];
 
 function App() {
-
-
   return (
-    <section className='d-flex justify-content-around flex-wrap'>
-      
-     
-      { data.map((element)=>{
-        
-        return(
-          <Card element={element}/>
-        )
-      })
-     }
-
+    <section className="d-flex justify-content-around flex-wrap">
+      {data.map((element) => {
+        return <Card element={element} />;
+      })}
     </section>
-  )
+  );
 }
 
-export default App
-
-const add = (a,b)=>{ return a+b};
-
-add(6,7)
-add(8,9)
+export default App;
